b222abd64d7c4cb001eeade3f8d24025b3631849
Yanke Guo <guoyk@pagoda.com.cn>
2019-04-16 11:09:41 +0800

    add filebeat

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@
 set -e
 set -u
 
-# recreate volumes
+# create volumes
 docker volume create logs-central
 docker volume create logs-app
 
-# recreate elasticsearch
+# create elasticsearch
 docker run -d --name elasticsearch -e "discovery.type=single-node" elasticsearch:6.7.1
 
 # build image for app
@@ -23,5 +23,28 @@ cd logtubed && \
     docker build -t logtubed . && \
     cd ..
 
-# recreate logtubed
+# build image for filebeat
+cd filebeat && \
+    docker build -t filebeat . && \
+    cd ..
+
+# create logtubed <- elasticsearch
 docker run -d --name logtubed -v logs-central:/data/xlogd_local --link elasticsearch logtubed
+
+# create app <- logtubed
+docker run -d --name app -v logs-app:/usr/local/tomcat/logs --link logtubed app
+
+# create filebeat <- logtubed
+docker run -d --name filebeat -v logs-app:/var/www/logs --link logtubed filebeat
+
+# sleep 10
+sleep 10
+
+# print logs
+docker logs elasticsearch
+
+docker logs logtubed
+
+docker logs app
+
+docker logs filebeat
